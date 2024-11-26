702665f983b06ee2c3700a45712937cdb3f76ba8
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-11-26 21:07:28 +0100

    First commit

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace basics.Controllers;
+
+public class CourseController:Controller {
+  public string Index() {
+    return "course/index";
+  }
+  public string List() {
+    return "course/list";
+  }
+}
